839d5667fc6cdc4b859a54a59a1bf408e4306f72
BuiDucNam <namkhang714@gmail.com>
2021-06-08 18:14:09 +0700

    Add TodoList function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,69 @@
 import React, { Component } from 'react';
 import './App.css';
 import TodoList from './components/TodoList/TodoList';
-import TrafficLight from './components/TrafficLight/TrafficLight';
+// import TrafficLight from './components/TrafficLight/TrafficLight';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newItem: "",
+      todoList : [
+        {title: 'Go to school',  isComplete: true},
+        {title: 'Go to bed'},
+        {title: 'Go to lunch',  isComplete: true}
+      ],
+    }
+  }
+
+  onChangeItem(item) {
+
+    this.setState({
+      todoList : this.state.todoList.map((todo) => {
+        return todo === item ? {...todo, isComplete: !item.isComplete} : {...todo};
+      })
+    });
+  }
+
+  onKeyUp = (event) => {
+    // let text = event.target.value;
+    if(event.keyCode === 13){
+      if(event.target.value.trim() !== ""){
+        this.setState({
+          newItem: "",
+          todoList : [
+            {title: event.target.value, isComplete: false},
+            ...this.state.todoList
+          ]
+        });
+      }
+    }
+  }
+
+  onChange = (event) => {
+    this.setState({
+      newItem: event.target.value,
+  });
+  }
+
   render(){
+    let { todoList, newItem } = this.state;
     return (
       <div className="App">
-        <TrafficLight />
+            <div className="header">
+              <i className="fas fa-check"></i>
+              <input 
+                placeholder="Add a item here" 
+                onKeyUp={this.onKeyUp} 
+                value={newItem} 
+                onChange={this.onChange}  
+              />
+            </div>
+       {
+         todoList.map((item, index) => 
+          <TodoList key={index} item={item} onClick = {() => this.onChangeItem(item)}/>
+         )
+       }
       </div>
     );
   }
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,9 +1,11 @@
 import './TodoList.css';
 import classnames from "classnames";
 const TodoList = (props) => {
-    const {item} = props;
+    const {item, onClick} = props;
+    
     return(
         <div className= {classnames('TodoList', {"TodoList-Complete" : item.isComplete})}>
+            <i onClick={onClick} className="fas fa-check-circle" style={ item.isComplete !== true ? {color: 'black'} : {color: 'green'}}></i>
             <p>{item.title}</p>
         </div>
     );
